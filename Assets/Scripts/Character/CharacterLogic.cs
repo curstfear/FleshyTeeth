be970b8eb910d9bbd0e8443109c6854ab084413f
Michael <kateshii16@gmail.com>
2024-05-22 22:38:15 +0300

    Поменял систему управления, добавил InputActions.

---
+++ Assets/Scripts/Character/CharacterLogic.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLogic : MonoBehaviour
+{
+    [SerializeField] private float _speed = 7f;
+    private CharacterInputActions _characterInputActions;
+
+    private Rigidbody2D _rigidBody;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _characterInputActions = new CharacterInputActions();
+        _characterInputActions.Enable();
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        Vector2 inputVector = GetMovementVector();
+        _rigidBody.MovePosition(_rigidBody.position + inputVector * (_speed * Time.fixedDeltaTime));
+    }
+
+    private Vector2 GetMovementVector()
+    {
+        Vector2 inputVector = _characterInputActions.Character.Movement.ReadValue<Vector2>();
+        return inputVector;
+    }
+}
